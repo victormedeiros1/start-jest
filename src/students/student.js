fbc0da1492980bde1bb746d2b27f9a4eff5fa1ee
José Victor <josevictordev@gmail.com>
2023-04-20 13:07:03 -0300

    test: initial tests using students

--- src/students/student.js
+++ src/students/student.js
@@ -1,3 +1,22 @@
-export const student = {
-  sum: (notes) => null,
+/* 
+  Regras estão no arquivo de testes
+*/
+
+module.exports = {
+  sum: (notes) => {
+    // Regra 1
+    if (Math.min(...notes) === 0) {
+      return 0;
+    }
+
+    // Regra 2
+    const result = notes.reduce((acc, note) => {
+      // Regra 4
+      acc += note > 9 ? note * 2 : note;
+      return acc;
+    });
+
+    // Regra 3
+    return notes.length > 5 ? result * 2 : result;
+  },
 };
--- src/students/students.spec.js
+++ src/students/students.spec.js
@@ -1,8 +1,17 @@
-// Devemos conseguir somar as notas dos alunos
-// Se caso uma nota for zero, a soma deve retornar zero
-// Caso o aluno tenha mais de cinco notas a soma deve ser multiplicada por 2
+/**
+ * Teste com TDD -> escrever os testes depois o código a ser testado.
+ *
+ * Regra 1 - Devemos conseguir somar as notas dos alunos.
+ * Regra 2 - Se caso uma nota for zero, a soma deve retornar zero.
+ * Regra 3 - Caso o aluno tenha mais de cinco notas a soma deve ser multiplicada por 2.
+ */
 
-import { student } from "./student";
+/**
+ * Teste sem TDD -> escrever o código a ser testado depois os testes
+ * Regra 4 - Caso a nota seja maior que 9 deve ser dobrada.
+ */
+
+const student = require("./student");
 
 describe("Testes para alunos", () => {
   it("Deve retornar 10 quando as notas forem 5, 3 e 2", () => {
@@ -12,11 +21,16 @@ describe("Testes para alunos", () => {
 
   it("Deve retornar zero quando uma das notas for zero", () => {
     const notes = [1, 2, 3, 0];
-    expect(stundent.sum(notes)).toEqual(0);
+    expect(student.sum(notes)).toEqual(0);
   });
 
   it("Deve dobrar a nota quando o aluno tiver mais de cinco notas", () => {
     const notes = [1, 2, 3, 4, 5, 6, 7];
-    expect(students.sum(notes)).toEqual(64);
+    expect(student.sum(notes)).toEqual(56);
+  });
+
+  it("Deve dobrar a nota se ela for maior que 9", () => {
+    const notes = [4, 5, 10];
+    expect(student.sum(notes)).toEqual(29);
   });
 });
